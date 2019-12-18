a268aa6da8d2429dcb97612a980c83329de9c917
John Heifetz <skidheif@gmail.com>
2019-12-18 14:35:35 +0300

    add if operators

--- Booleans/src/Loader.java
+++ Booleans/src/Loader.java
@@ -4,18 +4,24 @@ public class Loader {
   public static void main(String[] args) {
     int milkAmount = 200; // ml
     int powderAmount = 5; // g
-    int eggsCount = 3; // items
+    int eggsCount = 4; // items
     int sugarAmount = 5; // g
     int oilAmount = 30; // ml
     int appleCount = 8; // items
 
     //powder - 400 g, sugar - 10 g, milk - 1 l, oil - 30 ml
-    System.out.println("Pancakes");
+    if (powderAmount >= 400 && sugarAmount >= 10 && milkAmount >= 1 && oilAmount >= 30) {
+      System.out.println("Pancakes");
+    }
 
     //milk - 300 ml, powder - 5 g, eggs - 5
-    System.out.println("Omelette");
+    if (milkAmount >= 300 && powderAmount >= 5 && eggsCount >= 5) {
+      System.out.println("Omelette");
+    }
 
     //apples - 3, milk - 100 ml, powder - 300 g, eggs - 4
-    System.out.println("Apple pie");
+    if (appleCount >= 3 && milkAmount >= 100 && eggsCount >= 4) {
+      System.out.println("Apple pie");
+    }
   }
 }
