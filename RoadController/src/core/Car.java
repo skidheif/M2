23e00b961257d5253800f3dff20298808ddba4b5
John Heifetz <skidheif@gmail.com>
2019-12-20 17:04:21 +0300

    done lesson6 task1

--- RoadController/src/core/Car.java
+++ RoadController/src/core/Car.java
@@ -20,4 +20,45 @@ public class Car
             special + "Автомобиль с номером " + number +
             ":\n\tВысота: " + height + " мм\n\tМасса: " + weight + " кг";
     }
+
+    //В классе Car проекта RoadController создать геттеры и сеттеры.
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public boolean isHasVehicle() {
+        return hasVehicle;
+    }
+
+    public void setHasVehicle(boolean hasVehicle) {
+        this.hasVehicle = hasVehicle;
+    }
+
+    public boolean isSpecial() {
+        return isSpecial;
+    }
+
+    public void setSpecial(boolean special) {
+        isSpecial = special;
+    }
 }
